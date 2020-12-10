187cdbe54ef9b69ee1ab216e7f353c116b4b8815
unclickifet <75220393+unclickifet@users.noreply.github.com>
2020-12-10 22:59:09 +0100

    ok

---
+++ bot.py
@@ -0,0 +1,10 @@
+from telegram.ext import Updater
+updater = Updater(token='1295611507:AAHLGyAKby-ilZijQhsEelNYhsT9ELYaZbg')
+
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hola soy el bot de Oscar R y estoy en prueba")
