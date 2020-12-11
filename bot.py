b2ab857f07b05f994474aeeed3253a36895d6132
unclickifet <unclickifet@gmail.com>
2020-12-11 21:15:29 +0100

    ok

--- bot.py
+++ bot.py
@@ -16,3 +16,9 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
