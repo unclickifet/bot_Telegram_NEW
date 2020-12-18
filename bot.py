b64147375b8877fce2987ce550ffd98c698682ad
unclickifet <75220393+unclickifet@users.noreply.github.com>
2020-12-18 09:58:59 +0100

    ok

--- bot.py
+++ bot.py
@@ -1,17 +1,19 @@
 from telegram.ext import Updater
 from telegram.ext import MessageHandler, Filters
+import logging
+from telegram.ext import CommandHandler
 
 updater = Updater(token='1295611507:AAHLGyAKby-ilZijQhsEelNYhsT9ELYaZbg', use_context=True)
 
 dispatcher = updater.dispatcher
 
-import logging
+# Habilitar registro
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hola soy el bot de Oscar R y estoy en prueba")
 
-from telegram.ext import CommandHandler
+# Activamos CommandHandler
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
