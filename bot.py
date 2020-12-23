71b9b8bdb3e3559ad21d661aad1652fffda7a353
unclickifet <75220393+unclickifet@users.noreply.github.com>
2020-12-23 12:43:57 +0100

    ok

--- bot.py
+++ bot.py
@@ -27,10 +27,19 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
-
+updater.idle()
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
+
+def caps(bot, update, args):
+    text_caps = ' '.join(args).upper()
+    bot.send_message(chat_id=update.message.chat.id, text=text_caps)
+
+caps_handler = CommandHandler('caps', caps, pass_args=True)
+dispatcher.add_handler(caps_handler)
+
+
